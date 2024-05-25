6292d6b71b84c4af66f6f916136d4db707428628
idilTugba <idilgolcuk@gmail.com>
2024-05-25 18:31:58 +0300

    edit file names

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./asset/styles/app.scss";
 import { Provider } from "react-redux";
 import store from "./lib/redux/Store";
-import { MultiSelectComponent } from "@components/multiSelect/Index";
+import { MultiSelectComponent } from "@components/multiSelect";
 
 function App() {
   return (
